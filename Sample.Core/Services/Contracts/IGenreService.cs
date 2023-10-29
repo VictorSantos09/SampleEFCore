47abb7733787f2f171e48856ab5734325197e1e6
Victor Santos <victorluizsantos49@gmail.com>
2023-10-28 22:07:50 -0300

    refac front-end + adição snackbar

--- Sample.Core/Services/Contracts/IGenreService.cs
+++ Sample.Core/Services/Contracts/IGenreService.cs
@@ -1,7 +1,7 @@
 ﻿using Sample.Core.Models;
 
 namespace Sample.Core.Services.Contracts;
-internal interface IGenreService : IService<GenreModel>
+public interface IGenreService : IService<GenreModel>
 {
     public Task<GenreModel> GetByNameAsync(string name);
 }
